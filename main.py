58d827017d5344444b3da2377e9668aa98f89b84
abla-ha <ja_hagani@esi.dz>
2024-09-17 00:08:29 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from app.core.database import engine, Base
+from app.models import user  # Import models
+from app.api.v1 import auth, llm_chat,crop
+
+# Create FastAPI app instance
+app = FastAPI()
+
+# Create the database tables
+Base.metadata.create_all(bind=engine)
+
+# Include the auth router
+app.include_router(auth.router)
+app.include_router(llm_chat.router, prefix="/llm-chat", tags=["LLM Chat"])
+app.router.include_router(crop.router, prefix="/crop", tags=["crop"])
+# Simple route for testing
+@app.get("/")
+def read_root():
+    return {"message": "Hello, CropIntel!"}
