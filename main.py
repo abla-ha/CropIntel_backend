75735c6172e21417437da0ae56d42bf53819bad4
abla-ha <ja_hagani@esi.dz>
2024-09-17 22:52:11 +0100

    disease-detection

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.core.database import engine, Base
 from app.models import user  # Import models
-from app.api.v1 import auth, llm_chat,crop
+from app.api.v1 import auth, llm_chat,crop, pest_disease
 
 # Create FastAPI app instance
 app = FastAPI()
@@ -13,6 +13,9 @@ Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(llm_chat.router, prefix="/llm-chat", tags=["LLM Chat"])
 app.router.include_router(crop.router, prefix="/crop", tags=["crop"])
+# Include pest and disease detection routes
+app.include_router(pest_disease.router, prefix="/api/v1/pest_disease")
+
 # Simple route for testing
 @app.get("/")
 def read_root():
